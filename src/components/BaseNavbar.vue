bf05e5e26bd7fd4078c045d74bf36403143c0356
kevinhws <kevinshu1995@gmail.com>
2023-07-29 01:56:55 +0800

    feat: 🎸 update navbar

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -1,13 +1,24 @@
 <script setup>
-import { useNav } from '@/stores/nav'
 import { ref, watch, computed } from 'vue'
 import { useElementSize } from '@vueuse/core'
+import { useRouter } from 'vue-router'
+import { useNav } from '@/stores/nav'
 
+const router = useRouter()
 const storeNav = useNav()
 const sections = computed(() => storeNav.sections)
 const elNav = ref(null)
 
 const { height } = useElementSize(elNav)
+
+function onClickLogo() {
+  router.push('/')
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+}
 
 watch(height, () => {
   if (
@@ -24,56 +35,16 @@ watch(height, () => {
   <header class="sticky top-0" ref="elNav">
     <div class="navbar bg-base-100">
       <div class="navbar-start">
-        <a class="btn btn-ghost normal-case text-xl">Logo</a>
+        <button class="btn btn-ghost normal-case text-xl" @click="onClickLogo">Logo</button>
       </div>
-      <div class="navbar-center hidden md:flex">
+      <div class="navbar-end flex">
         <ul class="menu menu-horizontal px-1 space-x-2">
-          <li v-for="(section, i) in sections" :key="section.hash">
-            <router-link custom v-slot="{ isActive, href, navigate }" :to="`/#${section.hash}`">
-              <a
-                :href="href"
-                @click="navigate"
-                :class="$route.fullPath === href ? 'text-red-400' : ''"
-                >{{ section.text }}
-              </a>
+          <li v-for="section in sections" :key="section.hash">
+            <router-link :to="`/#${section.hash}`">
+              {{ section.text }}
             </router-link>
           </li>
         </ul>
-      </div>
-      <div class="navbar-end">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content right-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li v-for="(section, i) in sections" :key="section.hash">
-              <router-link custom v-slot="{ isActive, href, navigate }" :to="`/#${section.hash}`">
-                <a
-                  :href="href"
-                  @click="navigate"
-                  :class="$route.fullPath === href ? 'text-red-400' : ''"
-                  >{{ section.text }}
-                </a>
-              </router-link>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   </header>
