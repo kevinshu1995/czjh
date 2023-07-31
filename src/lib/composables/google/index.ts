5cf833500f9dcc938017297b63e6f7f6d542724c
kevinhws <kevinshu1995@gmail.com>
2023-08-01 00:15:56 +0800

    feat: 🎸 sheet data format

--- src/lib/composables/google/index.ts
+++ src/lib/composables/google/index.ts
@@ -7,7 +7,33 @@ const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY
 const SPREADSHEET_ID = import.meta.env.VITE_GOOGLE_SPREADSHEET_ID
 const DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4']
 
-const sheet = {
+type SheetName = {
+  staff: string
+  runDown: string
+  specialIntro: string
+  instrument: string
+}
+
+interface SheetStaff {
+  上台: 'TRUE' | ''
+  名稱: string
+  樂器: string
+  屆數: number
+}
+
+interface SheetInstrument {
+  id: string
+  group: string
+  樂器: string
+  instrument: string
+}
+
+interface SheetStaffWithInstrument extends SheetStaff {
+  instrumentGroup: number
+  instrumentId: number
+}
+
+const sheet: SheetName = {
   staff: '演出人員',
   runDown: '流程',
   specialIntro: '介紹',
@@ -130,7 +156,39 @@ export default function useGoogle() {
 
       const formattedData = SHEET_NAMES.map((sheetName: string, index: number) => {
         return formatSheetData(sheetName, response?.result?.valueRanges?.[index]?.values || [])
-      })
+      }).reduce((all: any, sheetData: any) => {
+        const keysOfSheet = Object.keys(sheet) as Array<keyof SheetName>
+        const engName =
+          keysOfSheet.find((key) => sheet[key] === sheetData.sheetName) || sheetData.sheetName
+        all[engName] = sheetData.table
+        return all
+      }, {})
+
+      formattedData.staff = formattedData.staff
+        .map((person: SheetStaff): SheetStaffWithInstrument => {
+          const { group, id } = data.instrument.find(
+            (instrument: SheetInstrument) => instrument.instrument === person['樂器']
+          )
+
+          return {
+            ...person,
+            instrumentGroup: Number(group),
+            instrumentId: Number(id)
+          }
+        })
+        .sort((a: SheetStaffWithInstrument, b: SheetStaffWithInstrument) => {
+          if (a.instrumentGroup !== b.instrumentGroup) return a.instrumentGroup - b.instrumentGroup
+          if (a.instrumentId !== b.instrumentId) return a.instrumentId - b.instrumentId
+          if (a['屆數'] === 0 || a['屆數'] === undefined) return 1
+          if (b['屆數'] === 0 || b['屆數'] === undefined) return -1
+          return a['屆數'] - b['屆數']
+        })
+        .reduce((all: SheetStaffWithInstrument[][], person: SheetStaffWithInstrument) => {
+          if (all[person.instrumentId] === undefined) all[person.instrumentId] = []
+          all[person.instrumentId].push(person)
+          return all
+        }, [])
+        .filter((item: any) => item !== undefined)
 
       // cache data
       setFetchAllSheets(formattedData)
@@ -147,7 +205,7 @@ export default function useGoogle() {
 
   function formatSheetData(sheetName: string, data: string[][]) {
     const [head, comment, ...body] = data
-    return {
+    const sheetData = {
       sheetName,
       table: body.map((row) => {
         return row.reduce((rowObject: any, columnValue, columnIndex) => {
@@ -156,7 +214,39 @@ export default function useGoogle() {
         }, {})
       })
     }
-  }
+    if (sheetName === sheet.staff) {
+      sheetData.table = sheetData.table
+        .filter((item) => item['上台'] === 'TRUE' && item['名稱'] && item['樂器'])
+        .map((item) => {
+          if (item['屆數'] === undefined) return item
+          item['屆數'] = Number(item['屆數'])
+          return item
+        })
+    }
 
-  return { fetchAllSheets }
+    if (sheetName === sheet.runDown) {
+      sheetData.table = sheetData.table
+        .filter((item) => item['公開活動'] === 'TRUE')
+        .sort((a, b) => a['順序'] - b['順序'])
+    }
+
+    if (sheetName === sheet.specialIntro) {
+      sheetData.table = sheetData.table.map((row) => {
+        return {
+          ...row,
+          介紹: row['介紹']?.split('\n') || []
+        }
+      })
+    }
+
+    if (sheetName === sheet.instrument) {
+      sheetData.table = sheetData.table.sort((a, b) => {
+        if (a['group'] === b['group']) return a.id - b.id
+        return a['group'] - b['group']
+      })
+    }
+
+    return sheetData
+  }
+  return { fetchAllSheets, sheet }
 }
--- src/stores/google.ts
+++ src/stores/google.ts
@@ -4,31 +4,9 @@ import { onMounted, ref, computed } from 'vue'
 import useGoogleComposable from '@/lib/composables/google'
 
 export const useGoogle = defineStore('google', () => {
-  const { fetchAllSheets } = useGoogleComposable()
+  const { fetchAllSheets, sheet: sheetEnum } = useGoogleComposable()
 
   const sheetsData = ref<any[] | null>(null)
-
-  const formattedSheet = computed(() => {
-    if (sheetsData.value === null) {
-      return {
-        staff: [],
-        runDown: [],
-        specialIntro: []
-      }
-    }
-
-    return sheetsData.value.map((sheet: { sheetName: string; table: any[] }) => {
-      if (sheet.sheetName === '演出人員') {
-        return sheet.table.filter((item) => item['上台'] === 'TRUE' && item['名稱'] && item['樂器'])
-      }
-      if (sheet.sheetName === '流程') {
-        return sheet.table.filter((item) => item['公開活動'] === 'TRUE')
-      }
-      if (sheet.sheetName === '介紹') {
-        return sheet.table
-      }
-    })
-  })
 
   onMounted(async () => {
     const { data, error } = await fetchAllSheets()
@@ -39,8 +17,8 @@ export const useGoogle = defineStore('google', () => {
     }
 
     sheetsData.value = data
-    console.log('data', formattedSheet.value)
+    console.log({ data })
   })
 
-  return { fetchAllSheets, formattedSheet }
+  return { fetchAllSheets }
 })
