d8abf9fb25fce69d708bed71038b09109e66576f
kevinhws <kevinshu1995@gmail.com>
2023-08-03 13:03:32 +0800

    feat: 🎸 staff section

--- src/views/HomeView/components/SectionIntro.vue
+++ src/views/HomeView/components/SectionIntro.vue
@@ -12,25 +12,28 @@ const helper = '*'
 </script>
 
 <template>
+  <!-- 演出人員 -->
   <div class="flex justify-center">
-    <ul class="space-y-12">
-      <li v-for="(group, i) in staff" :key="i" class="space-y-2">
-        <h3 class="text-3xl font-bold">{{ group[0]['樂器'] }}</h3>
-        <ul class="flex flex-wrap -mx-2">
-          <li v-for="(person, i) in group" class="px-2">
-            <div class="space-x-1 items-center">
-              <span v-if="i !== 0">/ </span>
-              <span>
-                {{ person['名稱'] }}
-              </span>
-              <span class="text-gray-400 text-xs">
-                {{ person['屆數'] === 0 ? helper : person['屆數'] }}
-              </span>
-            </div>
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <div class="space-y-12">
+      <ul class="space-y-12">
+        <li v-for="(group, i) in staff" :key="i" class="space-y-2">
+          <h3 class="text-3xl font-bold">{{ group[0]['樂器'] }}</h3>
+          <ul class="flex flex-wrap">
+            <li v-for="(person, i) in group" class="">
+              <div class="space-x-1 items-center">
+                <span v-if="i !== 0">、</span>
+                <span>
+                  {{ person['名稱'] }}
+                </span>
+                <span class="text-gray-300 text-xs">
+                  {{ person['屆數'] === 0 ? helper : '' }}
+                </span>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <p>{{ helper }} 協演</p>
+    </div>
   </div>
-  <p>{{ helper }} 協演</p>
 </template>
