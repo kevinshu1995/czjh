c6b02b9ae4bc70afd444f2800d2dfded2eeb75bb
kevinhws <kevinshu1995@gmail.com>
2023-08-03 14:26:14 +0800

    feat: 🎸 assets and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     container: {
-      padding: '8px',
+      padding: '1rem',
       center: true
     },
     extend: {}
