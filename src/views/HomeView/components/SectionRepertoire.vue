99887f57223bc5094296b977da00130288a005cc
kevinhws <kevinshu1995@gmail.com>
2024-08-17 10:39:57 +0800

    [change] add "說明" in repertoire seciton

--- src/views/HomeView/components/SectionRepertoire.vue
+++ src/views/HomeView/components/SectionRepertoire.vue
@@ -21,12 +21,15 @@ const runDown = computed(() => {
 <template>
   <div class="">
     <ul class="space-y-20">
-      <li v-for="item in runDown" :key="item.id" class="space-y-1 border-l-4 border-yellow11 pl-4">
-        <span class="text-yellow11 text-2xl">{{ item.order || '' }}</span>
-        <h3 class="text-3xl font-bold">
-          {{ item['曲目/活動'] }}
-        </h3>
-        <p class="text-gray-400">{{ item['小標題'] }}</p>
+      <li v-for="item in runDown" :key="item.id" class="space-y-4 border-l-4 border-yellow11 pl-4">
+        <div class="space-y-1">
+          <span class="text-yellow11 text-2xl">{{ item.order || '' }}</span>
+          <h3 class="text-3xl font-bold">
+            {{ item['曲目/活動'] }}
+          </h3>
+          <p class="text-gray-400">{{ item['小標題'] }}</p>
+        </div>
+        <p class="text-base-content">{{ item['說明'] }}</p>
       </li>
     </ul>
   </div>
